f7fcaa1d3783b8e9ae0e14f7c25708eff0a13ddf
sanjayagihanmadusanka <sanjayagihandesilva@gmail.com>
2025-09-21 13:26:24 +0530

    module 1 complete

--- index.js
+++ index.js
@@ -25,3 +25,18 @@ console.log("5" + 2);  // "52"
 console.log("5" - 2);  // 3
 // Boolean → Number (in arithmetic)
 console.log(true + 1); // 2
+
+let userNamr = prompt("enter your name: ");
+
+let fruit = "apple";
+switch (fruit) {
+  case "apple":
+  case "mango":
+  case "banana":
+    console.log("It’s a fruit!");
+    break;
+  default:
+    console.log("Not sure what it is...");
+}
+
+let canVote = (age >= 18) ? "Yes, you can vote" : "No, you are too young";
